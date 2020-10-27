e939469488d056cf1071325f650bdf1d31d7cd2d
Javier Ocaranza <javier.1889@gmail.com>
2020-10-27 20:40:57 -0300

    Add qp solver

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from setup import *
+from qpsolvers import solve_qp
+
+
 
 if __name__ == '__main__':
     A = np.array([[0.8, 0, 0.2],
@@ -8,6 +11,19 @@ if __name__ == '__main__':
     B = np.array([[1],
                   [0],
                   [0]])
-    N = 2
+    N = 7
     Aeq = Aeq(A, B, N)
-    print(Aeq)
+#from numpy import array, dot
+#from qpsolvers import solve_qp
+
+# if __name__ == '__main__':
+#     M = array([[1., 2., 0.], [-8., 3., 2.], [0., 1., 1.]])
+#     P = dot(M.T, M)  # quick way to build a symmetric matrix
+#     q = dot(array([3., 2., 3.]), M).reshape((3,))
+#     G = array([[1., 2., 1.], [2., 0., 1.], [-1., 2., -1.]])
+#     h = array([3., 2., -2.]).reshape((3,))
+#     A = array([1., 1., 1.])
+#     b = array([1.])
+#
+#     x = solve_qp(P, q, G, h, A, b)
+#     print("QP solution: x = {}".format(x))
--- setup.py
+++ setup.py
@@ -36,18 +36,36 @@ def Aeq(A, B, N):
 
     # Se crea la matriz completa
     Aeq = np.concatenate((Aeq_izquierda, Aeq_derecha), axis=1)
+
+    # Extensión de Aeq para considerar duk
+    columna = np.tile(zeros_vector, (N+1, N))
+    Aeq = np.concatenate((Aeq, columna), axis=1)
+    vector_aux1 = np.tile(np.transpose(zeros_vector), N+1)
+    vector_aux2 = np.array([[-1, 0, 1, 0]])
+    fila = np.concatenate((vector_aux1, vector_aux2), axis=1)
+    print(Aeq)
+    Aeq = np.concatenate((Aeq, fila), axis=0)
+
+
     return Aeq
 
 # Funcion que crea la matriz de H a utilizar en quadProg
 # Q es una matriz cuadrada de (NxN)
 # P es una matriz cuadrada de (NxN)
-# R es un vector de (Mx1) donde en cada fila se guarda el valor R_i
-def Hqp(Q, P, R):
+# R_i valor asociado a u
+
+def Hqp(Q, P, R_1, R_2):
+
 
     # Se consigue el tamaño de las matrices
     size_Q = np.shape(Q)[0]
     size_P = np.shape(P)[0]
     size_R = np.shape(R)[0]
+
+    # Matriz de ceros de (NxN)
+    zeros_matriz = np.zeros(size_Q, size_Q)
+
+
 
 
 
